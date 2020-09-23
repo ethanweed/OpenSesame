a13385309686aae38d1a22e2156d12c7bdaabff9
Ethan Weed <weed.ethan@gmail.com>
2020-09-24 00:18:05 +0200

    added fresh data and updated analysis script

--- Sternberg/scripts/plotSternberg.R
+++ Sternberg/scripts/plotSternberg.R
@@ -1,8 +1,15 @@
 
+rm(list = ls())
+
 library(tidyverse)
 
-df <- read.csv("/Users/ethan/Documents/GitHub/OpenSesame/Sternberg/data/jatos_results_20200914140649.csv")
-d <- select(df, block, correct, present, setSize, response_time)
+df <- read.csv("/Users/ethan/Documents/GitHub/OpenSesame/Sternberg/data/jatos_results_20200923231208.csv")
+
+# generate subject id's from timestamps
+df$id <- df$datetime
+levels(df$id) <- seq(1, length(levels(df$id)))
+
+d <- select(df, id, block, correct, present, setSize, response_time)
 
 experiment <- d %>%
   filter(block != "practice") %>%
@@ -12,7 +19,7 @@ correct <- experiment %>%
   filter(correct == 1) 
 
 collapsed <- correct %>%
-  group_by(setSize, present) %>%
+  group_by(setSize, present, id) %>%
   summarize(mRT = mean(rt))
 
 
@@ -22,16 +29,27 @@ meansMatrix <- collapsed %>%
   spread(key = present, value = m) %>%
   as.matrix()
 
+
 dfMeans <- as.data.frame(meansMatrix)
 colnames(dfMeans) <- c("Span", "NotPresent", "Present")
 
 dfMeans <- dfMeans %>%
   gather(key = Present, value = RT, -Span)
 
-model <- lm(RT ~ Span, dfMeans)
 
 ggplot(dfMeans, aes(Span, RT, color = Present)) +
   geom_point() +
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm") +
+  theme_classic()
+
+ggplot(correct, aes(setSize, rt, color = present)) +
+  geom_point() + 
+  geom_smooth(method = "lm") +
+  theme_bw()
 
 
+write.csv(correct, file = "/Users/ethan/Documents/GitHub/OpenSesame/Sternberg/data/Sternberg_data_correct.csv")
+
+present_absent <- select(correct, id,  present, rt)
+present_absent %>%
+  group_by(present)
